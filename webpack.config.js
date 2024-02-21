16e02a8d645e6c6166e81c0379e74f1d6b04da44
CocoJumbo0 <118237316+CocoJumbo0@users.noreply.github.com>
2024-02-21 15:16:01 -0500

    Removed dist folder from gitignore Modified title

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Template',
+            title: 'Template (change me!)',
         }),
     ],
     module: {
@@ -29,7 +29,7 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-            }
+            },
         ],
     },
     optimization: {
